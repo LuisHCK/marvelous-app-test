7198a28155d9f7daac259942314f00462044efa6
Luis J. Centeno <luisjcenteno@live.com>
2021-01-13 16:43:00 -0600

    Added http client

---
+++ src/services/api.service.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const API_KEY = process.env.MARVEL_APIKEY
+const SERVER_URL = process.env.SERVER_URL
+
+// Use Axios interceptor to inject apikey param
+const httpClient = axios.create({
+    baseURL: SERVER_URL,
+})
+
+httpClient.interceptors.request.use((config) => {
+    config.params = { ...config.params, apikey: API_KEY }
+    return config
+})
+
+export default httpClient
+
+/**
+ * Build endpoint path
+ * @param {String} base Base path
+ * @param  {...String} path Sub routes
+ * @returns {String} path
+ */
+export const buildPath = (...path) => path.join('/')
---
+++ src/services/comics.service.js
@@ -0,0 +1,54 @@
+import httpClient, { buildPath } from './api.service'
+const baseUrl = 'comics'
+
+/**
+ * Get Comic List
+ * @param {Object} params
+ * @returns {Promise}
+ */
+export const getComics = (params) => httpClient.get(baseUrl, { params })
+
+/**
+ * Retrieve a comic resource by id
+ * @param {Number|String} id Comic id
+ * @param {Object} params Request Params
+ * @returns {Promise}
+ */
+export const getComicById = (id, params = {}) =>
+    httpClient.get(buildPath(baseUrl, id), { params })
+
+/**
+ * Retrieve a comic's characters
+ * @param {Number|String} id Comic id
+ * @param {Object} params Request params
+ * @returns {Promise}
+ */
+export const getComicCharacters = (id, params = {}) =>
+    httpClient.get(buildPath(baseUrl, id, 'characters'), { params })
+
+/**
+ * Retrieve a comic's creators
+ * @param {Number|String} id Comic id
+ * @param {Object} params Request params
+ * @returns {Promise}
+ */
+export const getComicCreators = (id, params = {}) =>
+    httpClient.get(buildPath(baseUrl, id, 'creators'), { params })
+
+/**
+ * Retrieve a comic's stories
+ * @param {Number|String} id Comic id
+ * @param {Object} params Request params
+ * @returns {Promise}
+ */
+export const getComicStories = (id, params = {}) =>
+    httpClient.get(buildPath(baseUrl, id, 'stories'), { params })
+
+/**
+ * Retrieve a comic's events
+ * @param {Number|String} id Comic id
+ * @param {Object} params Request params
+ * @returns {Promise}
+ */
+export const getComicEvents = (id, params = {}) =>
+    httpClient.get(buildPath(baseUrl, id, 'events'), { params })
